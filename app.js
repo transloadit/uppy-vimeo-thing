6f076aa72239255fb430e5a310fdfb16c49c4bdf
Renée Kooi <renee@kooi.me>
2018-02-19 15:18:27 +0100

    GDrive

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Uppy = require('uppy/lib/core')
 const Tus = require('uppy/lib/plugins/Tus')
 const Dashboard = require('uppy/lib/plugins/Dashboard')
 const Webcam = require('uppy/lib/plugins/Webcam')
-const Instagram = require('uppy/lib/plugins/Instagram')
+const GoogleDrive = require('uppy/lib/plugins/GoogleDrive')
 const Vimeo = require('./Vimeo')
 
 css('uppy/dist/uppy.css')
@@ -19,7 +19,7 @@ if (typeof window !== 'undefined') {
       { id: 'description', name: 'Description', placeholder: 'My cool video' }
     ]
   })
-  .use(Instagram, { target: Dashboard, host: 'https://api2.transloadit.com/uppy-server' })
+  .use(GoogleDrive, { target: Dashboard, host: 'https://server.uppy.io' })
   .use(Webcam, { target: Dashboard })
 
   setClientID(localStorage.vimeoClientId || 'a7c30cc8ece39349ea055bf61bd51426fe2c50ef')
